78445551410291e9f57a1900b512913dbb4a1a1a
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-16 20:02:48 +0300

    05

---
+++ StacksAndQueues - Lab/05.PrintEvenEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.PrintEvenEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> numbersQueue = new Queue<int>(numbers);
+
+            List<int> numbersList = new List<int>();
+            while(numbersQueue.Count > 0)
+            {
+                int number = numbersQueue.Dequeue();
+                if(number % 2 == 0)
+                {
+                    numbersList.Add(number);
+                }
+            }
+            Console.WriteLine(String.Join(", ",numbersList));
+        }
+    }
+}
