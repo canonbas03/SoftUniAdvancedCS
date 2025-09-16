cef8d002ad35b9ba345ca2666fb23fe6ce0f6aa5
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-16 09:15:26 +0300

    Initial

---
+++ StacksAndQueuesLab/SoftUniAdvancedCS/StacksAndQueues/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SU!");
+            Console.WriteLine("Hello, SU!");
+        }
+    }
+}
