737708c7ec94d7644f8266e008da4a91842f1df7
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-16 19:52:24 +0300

    04

---
+++ StacksAndQueues - Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int openBracketIndex = stack.Pop();
+                    Console.WriteLine(expression.Substring(openBracketIndex,i - openBracketIndex + 1));
+                }
+            }
+        }
+    }
+}
