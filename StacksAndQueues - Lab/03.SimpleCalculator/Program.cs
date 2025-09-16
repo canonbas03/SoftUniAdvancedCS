380d0532ca5c26acdde3e12c671de8a88fac217f
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-16 19:13:27 +0300

    03

---
+++ StacksAndQueues - Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] expression = Console.ReadLine().Split().Reverse().ToArray();
+            Stack<string> stack = new Stack<string>(expression);
+            bool isAdd = true;
+            int total = 0;
+            while (stack.Count > 0)
+            {
+                string currentToken = stack.Pop();
+                if (currentToken == "+")
+                {
+                    isAdd = true;
+                }
+                else if (currentToken == "-")
+                {
+                    isAdd = false;
+                }
+                else
+                {
+                    int number = int.Parse(currentToken);
+                    if(!isAdd)
+                    {
+                        number *= -1;
+                    }
+                    total += number;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
