022ece3595ffb6f68d5d608d74f1277e2dad6da1
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-16 20:27:24 +0300

    0607

---
+++ StacksAndQueues - Lab/06.Supermarket/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = 0;
+            Queue<string> queue = new Queue<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if(input == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        peopleCount--;
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    peopleCount++;
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{peopleCount} people remaining.");
+        }
+    }
+}
---
+++ StacksAndQueues - Lab/07.HotPatato/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.HotPatato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queueKids = new Queue<string>(kids);
+            while (queueKids.Count > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    string safeKid = queueKids.Dequeue();
+                    queueKids.Enqueue(safeKid);
+                }
+                Console.WriteLine($"This is {queueKids.Dequeue()}");
+
+            }
+        }
+    }
+}
