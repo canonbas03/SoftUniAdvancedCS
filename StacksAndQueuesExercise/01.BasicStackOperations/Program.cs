fe131d30167e3a040a60387ad37555579ddff779
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-17 17:52:47 +0300

    01

--- StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
+++ StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
@@ -4,8 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            int[] parameters = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = parameters[0], s = parameters[1], x = parameters[2];
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if(stack.Count == 2) Console.WriteLine(0);
+            else
+            {
+                int minNumber = int.MaxValue;
+                bool found = false;
+                foreach (var num in stack)
+                {
+                    if(num == x)
+                    {
+                        found = true; 
+                        break;
+                    }
+                    if(num < minNumber) minNumber = num;
+                }
+                if(found) Console.WriteLine("true");
+                else Console.WriteLine(minNumber);
+            }
         }
     }
 }
