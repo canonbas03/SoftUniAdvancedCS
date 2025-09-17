bae53777c93f52868489054894d5fa4c1e325831
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-17 19:51:10 +0300

    04

---
+++ StacksAndQueuesExercise/04.FastFood/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            Queue<int> orders = ReadOrders();
+            Console.WriteLine(orders.Max());
+            while (orders.Count > 0)
+            {
+                if (orders.Peek() > foodQuantity)
+                    break;
+                foodQuantity -= orders.Dequeue();
+            }
+            if (orders.Count == 0) Console.WriteLine("Orders complete");
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+        }
+
+        private static Queue<int> ReadOrders()
+        {
+            IEnumerable<int> orders = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+            return new Queue<int>(orders);
+        }
+    }
+}
+/*
+499
+57 45 62 70 33 90 88 76
+*/
