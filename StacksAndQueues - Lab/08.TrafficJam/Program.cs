c0b13d49b6bfd604f70f8f4a8f105dfa685a4bc9
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-17 17:23:12 +0300

    Init

---
+++ StacksAndQueues - Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            int numberAllowed = int.Parse(Console.ReadLine());
+            int carsPassed = 0;
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < numberAllowed; i++)
+                    {
+                        if (queue.Count > 0)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            carsPassed++;
+                        }
+                    }
+                }
+                else queue.Enqueue(input);
+            }
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+        }
+    }
+}
---
+++ StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
