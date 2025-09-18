4e7dc4b202c1761a9c5e45a3d2b68e4b80047f36
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-18 22:08:37 +0300

    07

---
+++ StacksAndQueuesExercise/07.TruckTour/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumps = int.Parse(Console.ReadLine());
+            Queue<(int index,int fuel, int distance)> pumps = new Queue<(int ,int ,int )>();
+
+            for (int i = 0; i < petrolPumps; i++)
+            {
+                int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int fuel = values[0];
+                int distance = values[1];
+                pumps.Enqueue((i,fuel,distance));
+            }
+
+            int currentPump = pumps.Peek().index;
+        }
+    }
+}
