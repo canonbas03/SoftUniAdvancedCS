e488822665cc8e8a1c9eda2db3e46427493f660b
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-18 22:40:49 +0300

    08

--- StacksAndQueuesExercise/07.TruckTour/Program.cs
+++ StacksAndQueuesExercise/07.TruckTour/Program.cs
@@ -5,17 +5,44 @@
         static void Main(string[] args)
         {
             int petrolPumps = int.Parse(Console.ReadLine());
-            Queue<(int index,int fuel, int distance)> pumps = new Queue<(int ,int ,int )>();
+            Queue<(int index, int fuel, int distance)> pumps = new Queue<(int, int, int)>();
 
             for (int i = 0; i < petrolPumps; i++)
             {
                 int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
                 int fuel = values[0];
                 int distance = values[1];
-                pumps.Enqueue((i,fuel,distance));
+                pumps.Enqueue((i, fuel, distance));
             }
 
+            int currentFuel = 0;
+            int processed = 0;
             int currentPump = pumps.Peek().index;
+
+            while (true)
+            {
+                var pump = pumps.Dequeue();
+                currentFuel += pump.fuel - pump.distance;
+                pumps.Enqueue(pump);
+                
+                if(currentFuel < 0)
+                {
+                    processed = 0;
+                    currentFuel = 0;
+                    currentPump = pumps.Peek().index; 
+                }
+                else
+                {
+                    processed++;
+                }
+
+                if(processed == petrolPumps)
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine(currentPump);
         }
     }
 }
---
+++ StacksAndQueuesExercise/08.BalancedParantheses/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _08.BalancedParantheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string parantheses = Console.ReadLine();
+            Dictionary<char, char> map = new Dictionary<char, char>
+            {
+                ['{'] = '}',
+                ['['] = ']',
+                ['('] = ')'
+            };
+
+            if (IsBalanced(parantheses, map)) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+
+        }
+
+        private static bool IsBalanced(string text, Dictionary<char, char> dict)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var ch in text)
+            {
+                if (dict.ContainsKey(ch)) stack.Push(dict[ch]);
+                else if (stack.Pop() != ch) return false;
+            }
+            return stack.Count == 0;
+        }
+    }
+}
