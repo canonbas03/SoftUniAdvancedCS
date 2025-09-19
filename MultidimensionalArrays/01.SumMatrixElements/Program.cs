9067ef4df8845ac861986dd1272f68099944e39e
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-19 19:14:04 +0300

    01

---
+++ MultidimensionalArrays/01.SumMatrixElements/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace _01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            int[,] matrix =ReadMatrix(rows, cols);
+            int sum = MatrixSum(matrix);
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+
+        private static int MatrixSum(int[,] matrix)
+        {
+           int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+            return sum;
+        }
+
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                int[] colValues = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = colValues[j];
+                }
+            }
+            return matrix;
+        }
+    }
+}
