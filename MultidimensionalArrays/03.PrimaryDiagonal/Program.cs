71fedc90885bd808f9bc2408dd86a88848aa059c
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-19 19:39:47 +0300

    03

---
+++ MultidimensionalArrays/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            var matrix = ReadMatrix(size, size);
+
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += matrix[i,i];
+            }
+            Console.WriteLine(sum);
+        }
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+            return matrix;
+        }
+    }
+}
