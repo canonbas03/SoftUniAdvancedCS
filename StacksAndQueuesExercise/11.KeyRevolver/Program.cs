b222ac79a24ccdff8fc001de692d7db72ff8903b
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-19 09:40:37 +0300

    11

---
+++ StacksAndQueuesExercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace _11.KeyRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = int.Parse(Console.ReadLine());
+            int gunBarrel = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int intelligenceValue = int.Parse(Console.ReadLine());
+            int totalBullets = bullets.Count();
+
+            // locks front to back
+            // bullets back to front
+
+            Stack<int> bulletStack = new Stack<int>(bullets);
+            Queue<int> lockQueue = new Queue<int>(locks);
+
+            int currentBarrel = gunBarrel;
+            while (bulletStack.Count > 0 && lockQueue.Count > 0)
+            {
+                int currentBullet = bulletStack.Pop();
+                currentBarrel--;
+                if (currentBullet > lockQueue.First())
+                {
+                    Console.WriteLine("Ping!");
+                }
+                else
+                {
+                    Console.WriteLine("Bang!");
+                    lockQueue.Dequeue();
+                }
+
+                if (currentBarrel == 0 && bulletStack.Count >= gunBarrel)
+                {
+                    Console.WriteLine("Reloading!");
+                    currentBarrel = gunBarrel;
+                }
+            }
+            if (bulletStack.Count() < lockQueue.Count())
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {lockQueue.Count}");
+            }
+            else
+            {
+                int totalBulletPrice = (totalBullets - bulletStack.Count()) * price;
+                int earned = intelligenceValue - totalBulletPrice;
+                Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earned}");
+            }
+        }
+    }
+}
