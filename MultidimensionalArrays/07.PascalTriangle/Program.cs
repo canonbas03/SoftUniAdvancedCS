c42421ec4f1bc5a48907667a211d20e37f781693
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-20 14:02:58 +0300

    07

---
+++ MultidimensionalArrays/07.PascalTriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[size][];
+
+            pascalTriangle[0] = new long[] { 1 };
+            if (size > 1)
+            {
+                pascalTriangle[1] = new long[] { 1, 1 };
+
+                for (int row = 2; row < size; row++)
+                {
+                    pascalTriangle[row] = new long[row + 1];
+                    pascalTriangle[row][0] = 1;
+                    pascalTriangle[row][row] = 1;
+
+                    for (int col = 1; col < row ; col++)
+                    {
+                        long value = pascalTriangle[row - 1][col - 1] +
+                                     pascalTriangle[row - 1][col];
+
+                        pascalTriangle[row][col] = value;
+                    }
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(string.Join(" ", pascalTriangle[i]));
+            }
+
+        }
+    }
+}
