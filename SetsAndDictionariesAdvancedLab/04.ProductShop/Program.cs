97f50c2bbf06d3bbf0b082acad433cf8571c6242
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-22 15:33:39 +0300

    04

---
+++ SetsAndDictionariesAdvancedLab/04.ProductShop/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> shops = new();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Revision")
+            {
+                string[] values = input.Split(", ").ToArray();
+
+                string shop = values[0];
+                string product = values[1];
+                double price = double.Parse(values[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops[shop] = new Dictionary<string, double>();
+                }
+                shops[shop].Add(product, price);
+            }
+            foreach (var (shop, products) in shops)
+            {
+                Console.WriteLine($"{shop}->");
+                foreach (var (product, price) in products)
+                {
+                    Console.WriteLine($"Product: {product}, Price: {price}");
+                }
+            }
+
+        }
+    }
+}
