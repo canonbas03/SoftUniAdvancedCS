ea832c0cdcaea3b845a8ca801d7a4d1f3ce0c7bc
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-22 11:20:38 +0300

    02

---
+++ SetsAndDictionariesAdvancedLab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entries = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentGrades = new();
+
+            for (int i = 0; i < entries; i++)
+            {
+                string[] values = Console.ReadLine().Split();
+
+                string studentName = values[0];
+                decimal grade = decimal.Parse(values[1]);
+
+                if(!studentGrades.ContainsKey(studentName))
+                {
+                    studentGrades[studentName] = new List<decimal>();
+                }
+                studentGrades[studentName].Add(grade);
+            }
+
+            foreach (var student in studentGrades)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value)} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
+
+/*
+5
+George 6,00
+George 5,50
+George 6,00
+John 4,40
+Peter 3,30
+
+4
+Vlad 4,50
+Peter 3,00
+Vlad 5,00
+Peter 3,66
+ */
