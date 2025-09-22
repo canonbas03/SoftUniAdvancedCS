670611d4ddaa2d1af529ef15353667a3a4d53eee
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-22 10:22:28 +0300

    01

---
+++ SetsAndDictionariesAdvancedLab/01.CountSameValuesinArray/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.CountSameValuesinArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> counts = new Dictionary<double, int>();
+            foreach (var value in values)
+            {
+                if(!counts.ContainsKey(value))
+                {
+                    counts[value] = 0;
+                }
+                counts[value]++;
+            }
+
+            foreach (var pair in counts)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+        }
+    }
+}
