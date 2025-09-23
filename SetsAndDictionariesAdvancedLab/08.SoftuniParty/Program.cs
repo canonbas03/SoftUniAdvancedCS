d00371c1507089164f8b526e8ab3734ed84e05f5
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-23 10:44:52 +0300

    08

---
+++ SetsAndDictionariesAdvancedLab/08.SoftuniParty/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _08.SoftuniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vipGuests = new HashSet<string>();
+            var regularGuests = new HashSet<string>();
+            bool isParty = false;
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input == "PARTY")
+                {
+                    isParty = true;
+                    continue;
+                }
+                else if(isParty)
+                {
+                    vipGuests.Remove(input);
+                    regularGuests.Remove(input);
+                }
+                else
+                {
+                    if (char.IsDigit(input[0]))
+                    {
+                        vipGuests.Add(input);
+                    }
+                    else
+                    {
+                        regularGuests.Add(input);
+                    }
+                }
+            }
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
---
+++ SetsAndDictionariesAdvancedLab/Playground/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashSet = new HashSet<string>();
+
+            hashSet.Remove("abc");
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
