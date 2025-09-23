ebf3f8b28ee92c03a0809806c9ce3b34fd4a868e
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-23 16:43:52 +0300

    03

---
+++ SetsandDictionariesAdvancedExercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0], m = input[1];
+
+            List<int> commonElements = new List<int>();
+            HashSet<int> firstHS = ReadHS(n);
+            var secondHS = ReadHS(m);
+
+            foreach (var i in firstHS)
+            {
+                if (secondHS.Contains(i))
+                {
+                    commonElements.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", commonElements));
+        }
+
+        public static HashSet<int> ReadHS(int size)
+        {
+            HashSet<int> result = new HashSet<int>();
+            for (int i = 0; i < size; i++)
+            {
+                result.Add(int.Parse(Console.ReadLine()));
+            }
+            return result;
+        }
+    }
+}
---
+++ SetsandDictionariesAdvancedExercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var chemicals = new SortedSet<string>();
+            int counter = int.Parse(Console.ReadLine());
+            for (int i = 0; i < counter; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                foreach (string element in elements)
+                {
+                    chemicals.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicals));
+        }
+    }
+}
+
+/* 
+3
+Ge Ch O Ne
+Nb Mo Tc
+O Ne
+ */
