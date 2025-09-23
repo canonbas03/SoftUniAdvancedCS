e027ef3c928a0d321c6eaf8536ae988a4cf8b16c
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-23 09:40:36 +0300

    06

---
+++ SetsAndDictionariesAdvancedLab/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int namesCount = int.Parse(Console.ReadLine());
+            var uniqueNames = new HashSet<string>();
+            for (int i = 0; i < namesCount; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+            foreach (string name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
