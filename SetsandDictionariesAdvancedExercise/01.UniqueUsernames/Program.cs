0f42ad4d961db589b696335e2d6fe0695ed41fb2
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-23 11:45:13 +0300

    01

---
+++ SetsandDictionariesAdvancedExercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var usernames = new HashSet<string>();
+            int counter = int.Parse(Console.ReadLine());
+            for (int i = 0; i < counter; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+            foreach (string name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
