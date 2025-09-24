6e48515a35f3490b0a13d710c5d43ff0f854355c
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-24 17:08:18 +0300

    08

--- SetsandDictionariesAdvancedExercise/08.Ranking/Program.cs
+++ SetsandDictionariesAdvancedExercise/08.Ranking/Program.cs
@@ -5,10 +5,11 @@
         static void Main(string[] args)
         {
             var contestPasswords = new Dictionary<string, string>();
+            var userContest = new Dictionary<string, Dictionary<string, int>>();
             string input;
             while ((input = Console.ReadLine()) != "end of contests")
             {
-                string[] tokens = input.Split();
+                string[] tokens = input.Split(":");
                 string contest = tokens[0];
                 string password = tokens[1];
 
@@ -26,6 +27,33 @@
                 string password = tokens[1];
                 string studentName = tokens[2];
                 int points = int.Parse(tokens[3]);
+
+                if(contestPasswords.ContainsKey(course))
+                {
+                    if (contestPasswords[course] == password)
+                    {
+                        if(!userContest.ContainsKey(studentName))
+                        {
+                            userContest[studentName] = new Dictionary<string, int>();
+                        }
+                        if (!userContest[studentName].ContainsKey(course))
+                        {
+                            userContest[studentName][course] = 0;
+                        }
+                        userContest[studentName][course] = userContest[studentName][course] < points ? points : userContest[studentName][course];
+                    }
+                }
+            }
+            var bestCandidate = userContest.OrderByDescending(x => x.Value.Values.Sum()).First();
+            Console.WriteLine($"Best candidate is {bestCandidate.Key} with total {bestCandidate.Value.Values.Sum()} points.");
+            Console.WriteLine("Ranking:");
+            foreach (var candidate in userContest.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(candidate.Key);
+                foreach (var (course, points) in candidate.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {course} -> {points}");
+                }
             }
         }
     }
