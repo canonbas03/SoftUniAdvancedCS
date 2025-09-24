1477eb3a7e6e4fb8520ec4e86736914b9554e2e1
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-24 21:13:18 +0300

    Initial skeleton

---
+++ StreamsFilesAndDirectoriesLab/OddLines/OddLines.cs
@@ -0,0 +1,38 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+
+            int lineNum = 0;
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (reader == null)
+                {
+                    break;
+                }
+
+                if(lineNum % 2 == 1)
+                {
+                    writer.WriteLine(line);
+                }
+                lineNum++;
+
+            }
+        }
+    }
+}
