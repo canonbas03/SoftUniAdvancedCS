51e4eedbea60d81c2be3a3660198261c19e9e022
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-24 16:37:48 +0300

    08

---
+++ SetsandDictionariesAdvancedExercise/08.Ranking/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _08.Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contestPasswords = new Dictionary<string, string>();
+            string input;
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] tokens = input.Split();
+                string contest = tokens[0];
+                string password = tokens[1];
+
+                if(!contestPasswords.ContainsKey(contest))
+                {
+                    contestPasswords[contest] = string.Empty;
+                }
+                contestPasswords[contest] = password;
+            }
+            while((input =  Console.ReadLine()) != "end of submissions")
+            {
+                string[] tokens = input.Split("=>");
+                // C# Fundamentals=>fundPass=>Tanya=>350
+                string course = tokens[0];
+                string password = tokens[1];
+                string studentName = tokens[2];
+                int points = int.Parse(tokens[3]);
+            }
+        }
+    }
+}
