19ecc85661c1430f798cab4a5e7a7719fae3f98d
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-24 23:28:42 +0300

    03

--- StreamsFilesAndDirectoriesLab/WordCount/WordCount.cs
+++ StreamsFilesAndDirectoriesLab/WordCount/WordCount.cs
@@ -17,6 +17,40 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            var wordsByCount = new Dictionary<string, int>();
+
+            using (var reader = new StreamReader(wordsFilePath))
+            {
+                string[] words = reader.ReadLine().ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(w => w.ToLower()).ToArray();
+                foreach (string word in words)
+                {
+                    if (!wordsByCount.ContainsKey(word))
+                    {
+                        wordsByCount[word] = 0;
+                    }
+                }
+            }
+
+            using (var reader = new StreamReader(textFilePath))
+            {
+                string[] text = reader.ReadToEnd()
+                                      .ToLower()
+                                      .Split(new char[] { ' ', '-', '.', '!', ',', '?', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in text)
+                {
+                    if (wordsByCount.ContainsKey(word))
+                    {
+                        wordsByCount[word]++;
+                    }
+                }
+
+            }
+            using (var writer = new StreamWriter(outputFilePath))
+                foreach (var (word, amount) in wordsByCount.OrderByDescending(wc => wc.Value))
+                {
+                    writer.WriteLine($"{word} - {amount}");
+                }
         }
     }
 }
