a363c6da0c24ea007c7862c45a7b7981a99f7b22
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-24 18:50:19 +0300

    10

---
+++ SetsandDictionariesAdvancedExercise/10.ForceBook/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace _10.ForceBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sides = new Dictionary<string, List<string>>();
+            var users = new Dictionary<string, string>();
+            string input;
+            while ((input = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (input.Contains(" | "))
+                {
+                    string[] token = input.Split(" | ", 2, StringSplitOptions.None);
+
+                    string forceSide = token[0];
+                    string user = token[1];
+
+                    if (!sides.ContainsKey(forceSide))
+                    {
+                        sides[forceSide] = new List<string>();
+                    }
+                    if (!sides[forceSide].Contains(user))
+                        sides[forceSide].Add(user);
+                    if (!users.ContainsKey(user))
+                    {
+                        users[user] = forceSide;
+                    }
+
+
+                }
+                else if (input.Contains(" -> "))
+                {
+                    string[] token = input.Split(" -> ", 2, StringSplitOptions.None);
+
+                    string newForceSide = token[1];
+                    string user = token[0];
+
+                    string oldSide = string.Empty;
+
+                    if (users.ContainsKey(user))
+                    {
+                        oldSide = users[user];
+                        sides[oldSide].Remove(user);
+                    }
+                    if (!sides.ContainsKey(newForceSide))
+                    {
+                        sides[newForceSide] = new List<string>();
+                    }
+
+                    if (!sides[newForceSide].Contains(user))
+                        sides[newForceSide].Add(user);
+
+                    if (oldSide != newForceSide)
+                    {
+                        users[user] = newForceSide;
+                        Console.WriteLine($"{user} joins the {newForceSide} side!");
+                    }   
+                }
+            }
+
+            foreach (var (side, userCollection) in sides
+                                                   .Where(s => s.Value.Count > 0)
+                                                   .OrderByDescending(s => s.Value.Count)
+                                                   .ThenBy(s => s.Key))
+            {
+                Console.WriteLine($"Side: {side}, Members: {userCollection.Count}");
+                foreach (var user in userCollection.OrderBy(u => u))
+                {
+                    Console.WriteLine($"! {user}");
+                }
+            }
+        }
+    }
+}
