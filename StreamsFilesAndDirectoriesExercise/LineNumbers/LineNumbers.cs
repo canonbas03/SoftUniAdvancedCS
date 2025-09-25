311753ebed1ba414e09f48b22d033b048881188d
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-25 18:20:03 +0300

    02

--- StreamsFilesAndDirectoriesExercise/LineNumbers/LineNumbers.cs
+++ StreamsFilesAndDirectoriesExercise/LineNumbers/LineNumbers.cs
@@ -1,6 +1,10 @@
 ﻿namespace LineNumbers
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
     public class LineNumbers
     {
         static void Main()
@@ -13,7 +17,20 @@
 
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = FormatLine(lines[i], i);
+            }
+            File.WriteAllLines(outputFilePath, lines);
+        }
+
+        private static string FormatLine(string line, int index)
+        {
+            int letterCount = line.Where(x => char.IsLetter(x)).Count();
+            int punctCount = line.Where(x => char.IsPunctuation(x)).Count();
+            return $"Line {index + 1}: {line} ({letterCount})({punctCount})";
         }
     }
 }
