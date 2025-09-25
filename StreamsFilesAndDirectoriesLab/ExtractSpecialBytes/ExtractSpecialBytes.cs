6a32d009ae9f66dcd01708087184ad7c06c2efcb
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-25 10:04:18 +0300

    05

--- StreamsFilesAndDirectoriesLab/ExtractSpecialBytes/ExtractSpecialBytes.cs
+++ StreamsFilesAndDirectoriesLab/ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -1,7 +1,10 @@
 ﻿namespace ExtractSpecialBytes
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+
     public class ExtractSpecialBytes
     {
         static void Main()
@@ -15,6 +18,19 @@
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
+            HashSet<byte> bytes;
+            using (var reader = new StreamReader(bytesFilePath))
+            {
+                bytes = new HashSet<byte>();
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    bytes.Add(byte.Parse(line));
+                }
+            }
+            byte[] allBytes = File.ReadAllBytes(binaryFilePath).Where(b => bytes.Contains(b)).ToArray();
+
+            File.WriteAllBytes(outputPath, allBytes);
         }
     }
 }
