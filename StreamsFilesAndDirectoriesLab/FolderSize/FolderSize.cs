674ea12b344cd448d06c92a7beaf5b0b1a3161d4
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-25 13:57:30 +0300

    07

--- StreamsFilesAndDirectoriesLab/FolderSize/FolderSize.cs
+++ StreamsFilesAndDirectoriesLab/FolderSize/FolderSize.cs
@@ -14,6 +14,17 @@
 
         public static void GetFolderSize(string folderPath, string outputFilePath)
         {
+            string[] filePaths = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories);
+
+            long totalBytes = 0;
+            foreach (var file in filePaths)
+            {
+                FileInfo info = new FileInfo(file);
+                totalBytes += info.Length;
+            }
+
+            double totalKb = totalBytes / 1024.0;
+            File.WriteAllText(outputFilePath, totalKb + "KB");
         }
     }
 }
