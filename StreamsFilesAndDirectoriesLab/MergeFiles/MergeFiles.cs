d62134abf0633d4ac1d68f025ae19b42bf8c8207
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-25 09:03:23 +0300

    04

--- StreamsFilesAndDirectoriesLab/MergeFiles/MergeFiles.cs
+++ StreamsFilesAndDirectoriesLab/MergeFiles/MergeFiles.cs
@@ -15,6 +15,28 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
+            using (var read = new StreamReader(firstInputFilePath))
+            {
+                using (var readSecond = new StreamReader(secondInputFilePath))
+                {
+                    using (var write = new StreamWriter(outputFilePath))
+                    {
+                        while (!read.EndOfStream || !readSecond.EndOfStream)
+                        {
+                            if (!read.EndOfStream)
+                            {
+                                var lineOne = read.ReadLine();
+                                write.WriteLine(lineOne);
+                            }
+                            if (!readSecond.EndOfStream)
+                            {
+                                var lineTwo = readSecond.ReadLine();
+                                write.WriteLine(lineTwo);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
