369f6484d610297ded3b168482cf2f07a9ce72c4
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-28 17:09:22 +0300

    04

---
+++ FunctionalProgrammingLab/04.AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(", ")
+                .Select(double.Parse)
+                .Select(n => n * 1.2)
+                .Select(n => $"{n:f2}")
+                .ToList()
+                .ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
+
+// 1,38, 2,56, 4,4
