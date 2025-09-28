f192636dfebd703c3c28109ce8f3bf348c126428
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-28 17:21:54 +0300

    03

--- FunctionalProgrammingLab/03.CountUppercaseWords/Program.cs
+++ FunctionalProgrammingLab/03.CountUppercaseWords/Program.cs
@@ -4,12 +4,14 @@
     {
         static void Main(string[] args)
         {
+            Func<string, bool> isUpper = word => !string.IsNullOrEmpty(word) && char.IsUpper(word[0]);
             Console.ReadLine()
                 .Split(" ")
                 .Where(w => w.Length > 0)
-                .Where(w => char.IsUpper(w[0]))
+                .Where(isUpper)
                 .ToList()
                 .ForEach(w => Console.WriteLine(w));
+
         }
     }
 }
