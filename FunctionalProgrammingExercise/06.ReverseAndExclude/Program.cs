59af7df85291561c81a683f135c0bdf316cc6b1a
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-29 19:09:40 +0300

    06

---
+++ FunctionalProgrammingExercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _06.ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            int[] result = InReverse(numbers, x => x % n != 0);
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+
+        static int[] InReverse(int[] array, Func<int, bool> func)
+        {
+            List<int> list = new List<int>();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (func(array[i]))
+                    list.Add(array[i]);
+            }
+            return list.ToArray();
+        }
+    }
+}
