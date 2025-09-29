64e8349db3930f5d6421371cfaf3e090a6b3875d
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-29 13:39:09 +0300

    01

---
+++ FunctionalProgrammingExercise/01.ActionPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Iterate(names, n => Console.WriteLine(n));
+        }
+
+        static void Iterate(string[] array, Action<string> action)
+        {
+            foreach (string word in array)
+            {
+                action(word);
+            }
+        }
+    }
+}
