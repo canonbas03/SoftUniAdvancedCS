861080a2261eceafd8b3fc0238d2024f5e835896
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-29 13:50:07 +0300

    02

---
+++ FunctionalProgrammingExercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Iterate(names, n => Console.WriteLine($"Sir {n}"));
+        }
+
+        private static void Iterate(string[] names, Action<string> action)
+        {
+            foreach (string name in names)
+            {
+                action(name);
+            }
+        }
+    }
+}
