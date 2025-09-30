d10b5a1bf02b338be5044c43d0695a60112053bc
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-30 20:47:22 +0300

    10

--- FunctionalProgrammingExercise/09.Predicate party/Program.cs
+++ FunctionalProgrammingExercise/09.Predicate party/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace _09.Predicate_party
+﻿using System.Data;
+
+namespace _09.Predicate_party
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> peoples = Console.ReadLine().Split().ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Party!")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string criteria = tokens[1];
+                string argument = tokens[2];
+
+                Func<string, bool> condition = GetPredicate(criteria, argument);
+                if(command == "Remove")
+                {
+                    RemoveWhere(peoples, condition);
+                }
+                else if(command =="Double")
+                {
+                    DoubleWhere(peoples,condition);
+                }
+            }
+            if(peoples.Count > 0)
+            {
+                Console.WriteLine($"{string.Join(", ", peoples)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+
+        private static void DoubleWhere(List<string> peoples, Func<string, bool> condition)
+        {
+            for(int i = peoples.Count - 1;i >= 0; i--)
+            {
+                if (condition(peoples[i]))
+                {
+                    peoples.Insert(i, peoples[i]);
+                }
+            }
+        }
+
+        private static void RemoveWhere(List<string> peoples, Func<string, bool> condition)
+        {
+            for(int i = peoples.Count - 1; i >= 0; i--)
+            {
+                if (condition(peoples[i]))
+                {
+                    peoples.RemoveAt(i);
+                }
+            }
+        }
+
+        static Func<string, bool> GetPredicate (string type , string arg)
+        {
+            if(type == "StartsWith")
+            return x => x.StartsWith(arg);
+
+            else if(type == "EndsWith")
+            {
+                return x => x.EndsWith(arg);
+            }
+
+            else if(type == "Length")
+            {
+                return x => x.Length == int.Parse(arg);
+            }
+            return _ => false;
         }
     }
 }
---
+++ FunctionalProgrammingExercise/10.PartyReservationFilterModule/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _10.PartyReservationFilterModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine().Split().ToList();
+
+            Dictionary<string, Func<string, bool>> filters = new();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Print")
+            {
+                string[] tokens = input.Split(";",StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+                string condition = tokens[1];
+                string argument = tokens[2];
+
+                string key = $"{condition}_{argument}";
+                Func<string, bool> currentCondition = MakeCondition(condition, argument);
+                if (action == "Add filter")
+                {
+                    if (!filters.ContainsKey(key))
+                    {
+                        filters[key] = currentCondition;
+                    }
+                }
+                else if (action == "Remove filter")
+                {
+                    if (filters.ContainsKey(key))
+                    {
+                        filters.Remove(key);
+                    }
+                }
+            }
+            Func<string, bool> final = All(filters);
+            List<string> result = new List<string>();
+            foreach (string s in guests)
+            {
+                if(!final(s))
+                    result.Add(s);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+        static Func<string, bool> All(Dictionary<string, Func<string, bool>> dict)
+        {
+            return x =>
+            {
+                foreach (var (commandName, function) in dict)
+                    if (function(x)) return true;
+
+                return false;
+            };
+
+        }
+
+        private static Func<string, bool> MakeCondition(string con, string arg)
+        {
+            if (con == "Starts with")
+                return x => x.StartsWith(arg);
+            if (con == "Ends with")
+                return x => x.EndsWith(arg);
+            if (con == "Length")
+                return x => x.Length == int.Parse(arg);
+            if (con == "Contains")
+                return x => x.Contains(arg);
+            return _ => false;
+        }
+    }
+}
