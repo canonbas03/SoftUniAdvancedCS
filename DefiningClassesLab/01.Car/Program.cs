5a770c3c6d54974aec19bfaefe0907fd1f7ff520
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-02 19:53:14 +0300

    01

---
+++ DefiningClassesLab/01.Car/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "BMW",
+                Model = "OMG",
+                Year = 2031
+            };
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+
+        }
+    }
+}
