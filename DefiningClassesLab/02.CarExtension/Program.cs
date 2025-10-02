26e375b4be3c5562b5d2f89b2020e82378495143
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-02 20:57:09 +0300

    02

---
+++ DefiningClassesLab/02.CarExtension/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "Merc",
+                Model = "wow",
+                Year = 2026,
+                FuelQuantity = 50,
+                FuelConsumption = 10
+            };
+
+            car.Drive(50);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
