f8ba9b09f1b18cf28ba7a28363d793ebb37f9779
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-02 19:32:54 +0300

    11

--- FunctionalProgrammingExercise/11.TriFunction/Program.cs
+++ FunctionalProgrammingExercise/11.TriFunction/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int givenNumber = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> sumCheck = (name, n) =>
+            {
+                int sum = 0;
+                foreach (char ch in name)
+                {
+                    sum += (int)ch;
+                }
+                return sum >= givenNumber;
+            };
+
+            Action<string[], int, Func<string,int, bool>> final = (nameArr, num, check) =>
+            {
+                foreach (string name in nameArr)
+                {
+                    if(check(name, num))
+                    {
+                        Console.WriteLine(name);
+                        break;
+                    }
+
+                }
+            };
+            final(names, givenNumber, sumCheck);
         }
     }
 }
