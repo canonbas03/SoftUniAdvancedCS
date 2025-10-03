eab3ef1225b5b4caf7010a18bf191befe8031946
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-03 18:31:55 +0300

    03

---
+++ DefiningClassesExercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> people = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people.MaxBy(p => p.Age);
+        }
+    }
+}
---
+++ DefiningClassesExercise/03.OldestFamilyMember/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                Person person = new Person(name,age);
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
