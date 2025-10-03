7fc6705b82b74e21c0e2d519aad3cbc05f2ac652
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-03 16:08:46 +0300

    01

---
+++ DefiningClassesExercise/01.DefineAClassPerson/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+    }
+}
---
+++ DefiningClassesExercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Bob Marley";
+            p1.Age = 69;
+        }
+    }
+}
