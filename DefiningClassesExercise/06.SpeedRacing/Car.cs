2cebc9bf971a7611315a2cb7589e477b1cd4d0ba
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-03 20:30:59 +0300

    06

---
+++ DefiningClassesExercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.SpeedRacing
+{
+    internal class Car
+    {
+		private string model;
+		private double fuelAmount;
+		private double fuelConsumption;
+		private double traveledDistance;
+
+		public double TraveledDistance
+		{
+			get { return traveledDistance; }
+			set { traveledDistance = value; }
+		}
+
+
+		public double FuelConsumption
+		{
+			get { return fuelConsumption; }
+			set { fuelConsumption = value; }
+		}
+
+
+		public double FuelAmount
+		{
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}
+
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		public bool Drive(int distance)
+		{
+			double requiredFuel = this.FuelConsumption * distance;
+			if(this.FuelAmount >= requiredFuel)
+			{
+				this.FuelAmount -= requiredFuel;
+				this.TraveledDistance += distance;
+				return true;
+			}
+			
+			return false;
+		}
+	}
+}
---
+++ DefiningClassesExercise/06.SpeedRacing/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _06.SpeedRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> carByModel = new();
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                string model = tokens[0];
+                double fuelAmount = double.Parse(tokens[1]);
+                double fuelConsumption = double.Parse(tokens[2]);
+
+                carByModel[model] = new Car
+                {
+                    Model = model,
+                    FuelAmount = fuelAmount,
+                    FuelConsumption = fuelConsumption
+                };
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split();
+
+                string model = tokens[1];
+                int distance = int.Parse(tokens[2]);
+
+                if (!carByModel[model].Drive(distance))
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+            }
+
+            foreach (Car car in carByModel.Values)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TraveledDistance}");
+            }
+        }
+    }
+}
+/*
+2
+AudiA4 23 0,3
+BMW-M2 45 0,42
+Drive BMW-M2 56
+Drive AudiA4 5
+Drive AudiA4 13
+End
+ */
