fe470644635b1dfcf257be4fb106484918bfbbda
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-03 17:22:46 +0300

    02

---
+++ DefiningClassesExercise/02.CreatingConstructors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person(20);
+            Person p3 = new Person("Jorge",25);
+           
+        }
+    }
+}
