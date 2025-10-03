d101b7ebf388e69839ec2f0a988b2bfeebd3a985
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-03 19:26:52 +0300

    04

---
+++ DefiningClassesExercise/04.OpinionPoll/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DefiningClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            foreach (Person person in people
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
