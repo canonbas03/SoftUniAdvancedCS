e81931d4a8f383d65021c7506ffa8bc698c4042e
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-04 20:37:29 +0300

    05

---
+++ DefiningClassesExercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        public int Days { get; private set; }
+
+        public static int DaysBetweenDates(string date1, string date2)
+        {
+            DateTime firstDate = GetDate(date1);
+            DateTime secondDate = GetDate(date2);
+            DateTime GetDate(string date)
+            {
+                string[] tokens = date.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+                int year = int.Parse(tokens[0]);
+                int month = int.Parse(tokens[1]);
+                int day = int.Parse(tokens[2]);
+                DateTime dateTime = new DateTime(year,month,day);
+                return dateTime;
+            }
+            TimeSpan difference = secondDate - firstDate;
+            return Math.Abs(difference.Days);
+        }
+    }
+}
---
+++ DefiningClassesExercise/05.DateModifier/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.DateModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DateModifier.DaysBetweenDates("19 05 31", "2016 06 17")); 
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.DaysBetweenDates(date1, date2));
+        }
+    }
+}
