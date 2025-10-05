69874b58e06f7cf7765c860aecb92b94f8810399
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-05 20:58:53 +0300

    Custom ListClass

---
+++ WorkShop/CustomListClass/.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    internal class MyLinkedListNode
+    {
+        public int Value { get; set; }
+        public MyLinkedListNode Prev { get; set; }
+        public MyLinkedListNode Next { get; set; }
+
+        public MyLinkedListNode(int value)
+        {
+            this.Value = value;
+        }
+    }
+}
