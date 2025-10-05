065102278c193a963f20619daa2e53a8c6736e5e
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-05 15:18:31 +0300

    encapsulated

--- WorkShop/CustomListClass/CustomList.cs
+++ WorkShop/CustomListClass/CustomList.cs
@@ -20,8 +20,9 @@ namespace CustomListClass
         public CustomList(int size)
         {
             if (size <= 0)
+            {
                 throw new ArgumentException("Size must be greater than 0!!!");
-
+            }
             this._arr = new int[size];
         }
 
@@ -41,8 +42,7 @@ namespace CustomListClass
 
         public void Add(int element)
         {
-            if (this._arr.Length == this._count)
-                this.Grow();
+            this.GrowIfNecessary();
 
             this._arr[_count] = element;
             this._count++;
@@ -50,6 +50,7 @@ namespace CustomListClass
         public void RemoveAt(int index)
         {
             this.ValidateIndex(index);
+
             for (int i = index; i < this._count - 1; i++)
             {
                 this._arr[i] = this._arr[i + 1];
@@ -58,15 +59,37 @@ namespace CustomListClass
             this._count--;
         }
 
-        public void Grow()
+        public void InsertAt(int index, int value)
+        {
+            if (index == this._count) Add(value);
+            else
+            {
+                this.ValidateIndex(index);
+                this.GrowIfNecessary();
+
+                for (int i = this._count - 1; i >= index; i--)
+                {
+                    this._arr[i + 1] = this._arr[i];
+                }
+                this._arr[index] = value;
+                this._count++;
+            }
+        }
+
+        private void Grow()
         {
             int[] newArr = new int[this._arr.Length * 2];
             Array.Copy(this._arr, newArr, this._arr.Length);
 
             this._arr = newArr;
         }
+        private void GrowIfNecessary()
+        {
+            if (this._arr.Length == this._count)
+                this.Grow();
+        }
 
-        public void ValidateIndex(int index)
+        private void ValidateIndex(int index)
         {
             if (index < 0 || index >= this._count)
                 throw new IndexOutOfRangeException($"Index must be in [0, {this._count - 1}]!!!");
--- WorkShop/CustomListClass/Program.cs
+++ WorkShop/CustomListClass/Program.cs
@@ -5,18 +5,23 @@
         static void Main(string[] args)
         {
             CustomList customList = new CustomList();
+            customList.Add(2);
+            customList.Add(8);
+            Console.WriteLine(customList[0]);
 
-            for (int i = 0; i < 5; i++)
-            {
-                customList.Add(i);
-                Console.WriteLine(customList[i]);
-            }
-            customList.RemoveAt(2);
-            for (int i = 0; i < 4; i++)
-            {
-                Console.WriteLine(customList[i]);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    customList.Add(i);
+            //    Console.WriteLine(customList[i]);
+            //}
+            //customList.RemoveAt(2);
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine(customList[i]);
 
-            }
+            //}
+
+            
         }
     }
 }
