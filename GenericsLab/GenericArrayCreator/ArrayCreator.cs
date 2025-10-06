9db35aa13fc5591146dc8295ac4ffa179864d166
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-06 21:08:01 +0300

    method creation

--- GenericsLab/GenericArrayCreator/ArrayCreator.cs
+++ GenericsLab/GenericArrayCreator/ArrayCreator.cs
@@ -2,6 +2,14 @@
 {
     public class ArrayCreator
     {
-
+        public static T[] Create<T>(int size, T element)
+        {
+            T[] array = new T[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = element;
+            }
+            return array;
+        }
     }
 }
