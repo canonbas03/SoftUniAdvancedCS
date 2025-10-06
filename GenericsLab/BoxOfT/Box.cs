a5af497bc4e8f80b02681c92be87e2b87ac2a70d
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-06 20:57:00 +0300

    01

---
+++ GenericsLab/BoxOfT/Box.cs
@@ -0,0 +1,29 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public int Count => this.list.Count;
+        public Box()
+        {
+            list = new List<T>();
+        }
+        public void Add(T element)
+        {
+            this.list.Add(element);
+        }
+
+        public T Remove()
+        {
+            if(list.Count <= 0)
+            {
+                throw new ArgumentOutOfRangeException("No more elements!!!");
+            }
+            T element = this.list[list.Count - 1];
+            this.list.RemoveAt(list.Count - 1);
+            return element;
+
+        }
+    }
+}
---
+++ GenericsLab/BoxOfT/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> myBox = new Box<int>();
+
+            myBox.Add(11);
+            myBox.Add(22);
+
+            Console.WriteLine(myBox.Count);
+
+            Console.WriteLine(myBox.Remove());
+            Console.WriteLine(myBox.Remove());
+            Console.WriteLine(myBox.Count);
+
+        }
+    }
+}
