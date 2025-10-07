264b6d3c4ca2d8a6e083741ed7cb265abeaa130e
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-07 20:11:20 +0300

    01

---
+++ GenericsExercise/GenericBox/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericBox
+{
+    public class Box<T>
+    {
+        private readonly T _value;
+
+        public T Value => _value;
+
+        public Box(T value)
+        {
+            this._value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {Value}";
+        }
+    }
+}
---
+++ GenericsExercise/GenericBox/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+                var box = new Box<string>(text);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
