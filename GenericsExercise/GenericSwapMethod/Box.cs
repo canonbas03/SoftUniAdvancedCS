a3d2e677e9eef99125b108b875bc628167a0dbbb
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-07 20:55:31 +0300

    03

---
+++ GenericsExercise/GenericSwapMethod/Box.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericSwapMethod
+{
+
+    public class Box<T>
+    {
+        private readonly T _value;
+
+        public T Value => _value;
+
+        public Box(T value)
+        {
+            this._value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {Value}";
+        }
+    }
+
+}
---
+++ GenericsExercise/GenericSwapMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GenericSwapMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<Box<string>>(capacity: n);
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                var box = new Box<string>(value);
+                list.Add(box);
+            }
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Swap(list, indexes[0], indexes[1]);
+            foreach (Box<string> item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Swap<T>(List<T> list, int index1, int index2)
+        {
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
