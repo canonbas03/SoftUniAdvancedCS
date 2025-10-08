e63d29743fcf5fa2e112663321405e3e3ee3f840
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-08 19:11:18 +0300

    08

---
+++ GenericsExercise/MyThruple/CustomThruple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThruple
+{
+    public class CustomThruple<T1, T2, T3>
+    {
+        public T1 Item1 { get; }
+        public T2 Item2 { get; }
+        public T3 Item3 { get; }
+
+
+        public CustomThruple(T1 item1, T2 item2, T3 item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+    }
+}
---
+++ GenericsExercise/MyThruple/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MyThruple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            ProcessFirstLine(tokens);
+
+            tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            ProcessSecondLine(tokens);
+
+            tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            ProcessThirdLine(tokens);
+        }
+        static void ProcessFirstLine(string[] arr)
+        {
+            string fullName = $"{arr[0]} {arr[1]}";
+            string address = arr[2];
+            string town = string.Join(" ", arr.Skip(3));
+
+            var thruple = new CustomThruple<string, string, string>(fullName, address, town);
+            Console.WriteLine(thruple);
+        }
+
+        private static void ProcessSecondLine(string[] tokens)
+        {
+            string name = tokens[0];
+            int liters = int.Parse(tokens[1]);
+            bool isDrunk = tokens[2] == "drunk";
+
+            var thruple = new CustomThruple<string, int, bool>(name, liters, isDrunk);
+            Console.WriteLine(thruple);
+        }
+
+        static void ProcessThirdLine(string[] arr)
+        {
+            string name = arr[0];
+            double balance = double.Parse(arr[1]);
+            string bankName = arr[2];
+
+            var thruple = new CustomThruple<string, double, string>(name, balance, bankName);
+            Console.WriteLine(thruple);
+        }
+    }
+}
+/*
+Adam Smith Wallstreet New York
+Mark 18 drunk
+Karren 0,10 USBank
+ */
