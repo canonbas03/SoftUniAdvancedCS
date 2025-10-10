3fec12f03f7d6556f9422d99a69931be0ae5b982
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-10 19:57:52 +0300

    01

---
+++ IteratorsAndComparatorsExercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private readonly List<T> _list;
+        private int _index;
+        public ListyIterator(List<T> list)
+        {
+            this._list = list;
+        }
+
+        public bool Move()
+        {
+            if (!this.HasNext()) return false;
+
+            this._index++;
+            return true;
+        }
+        public bool HasNext() => this.IndexIsValid(this._index + 1);
+
+        public T Print()
+        {
+            if (!this.IndexIsValid(this._index)) throw new InvalidOperationException("Invalid Operation!");
+
+            return this._list[this._index];
+        }
+
+        private bool IndexIsValid(int index)
+        {
+            return index < this._list.Count;
+        }
+    }
+}
+/*
+Create Steve George
+HasNext
+Print
+Move
+Print
+END
+ */
---
+++ IteratorsAndComparatorsExercise/ListyIterator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ListyIterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            List<string> list = new List<string>(tokens.Skip(1));
+            var iterator = new ListyIterator<string>(list);
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                Process(input,iterator);
+            }
+        }
+
+        static void Process<T>(string input, ListyIterator<T> iterator)
+        {
+            try
+            {
+                if (input == "Move")
+                {
+                    bool result = iterator.Move();
+                    Console.WriteLine(result);
+                }
+                else if (input == "HasNext")
+                {
+                    bool result = iterator.HasNext();
+                    Console.WriteLine(result);
+                }
+                else if (input == "Print")
+                {
+                    T element = iterator.Print();
+                    Console.WriteLine(element);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
