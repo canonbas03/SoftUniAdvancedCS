36f7fef4202773f97df5b4dd226175fba652674b
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-11 21:21:34 +0300

    03

---
+++ IteratorsAndComparatorsExercise/StackIterator/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace StackIterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                ProcessCommand(input, stack);
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (int el in stack)
+                {
+                    Console.WriteLine(el);
+                }
+            }
+        }
+
+        static void ProcessCommand(string input, Stack<int> stack)
+        {
+            try
+            {
+                if (input == "Pop")
+                {
+                    stack.Pop();
+                }
+                else if (input.StartsWith("Push"))
+                {
+                    IEnumerable<int> numbers = input.Substring(5).Split(", ").Select(int.Parse);
+
+                    foreach (int number in numbers)
+                    {
+                        stack.Push(number);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ IteratorsAndComparatorsExercise/StackIterator/Stack.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+
+namespace StackIterator
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private T[] _arr;
+        public int Count { get; private set; }
+
+        public Stack()
+        {
+            this._arr = new T[4];
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+                throw new IndexOutOfRangeException("No elements");
+
+            T element = this._arr[--this.Count];
+            this._arr[this.Count] = default;
+            return element;
+        }
+        public void Push(T element)
+        {
+            if (this.Count == this._arr.Length)
+                Grow();
+
+            this._arr[this.Count++] = element;
+        }
+
+        private void Grow()
+        {
+            T[] newArray = new T[_arr.Length * 2];
+            Array.Copy(this._arr, newArray, this._arr.Length);
+            this._arr = newArray;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StackEnumerator(this._arr, this.Count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class StackEnumerator : IEnumerator<T>
+        {
+            private readonly T[] _arr;
+            private readonly int _count;
+
+            private int _index;
+
+            public StackEnumerator(T[] array, int count)
+            {
+                this._arr=array;
+                this._count = count;
+
+                Reset();
+            }
+
+            public T Current => this._arr[_index];
+
+            object IEnumerator.Current => this.Current;
+
+
+
+            public bool MoveNext()
+            {
+                if(this._index < 0) return false;
+
+                return --this._index >= 0;
+            }
+
+            public void Reset()
+            {
+                this._index = this._count;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
