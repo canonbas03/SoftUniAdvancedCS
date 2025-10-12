8ed22652e33f0f197c39401bed57c009ff39dd69
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-12 15:39:49 +0300

    07

---
+++ IteratorsAndComparatorsExercise/CustomComparator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomComparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            IComparer<int> comparer = Comparer<int>.Create((a, b) =>
+            {
+                int remainderComparison = Comparer<int>.Default.Compare(Math.Abs(a % 2), Math.Abs(b %2));
+                if (remainderComparison != 0) return remainderComparison;
+
+                return Comparer<int>.Default.Compare(a, b);
+            });
+            Array.Sort(numbers, comparer);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
