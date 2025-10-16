762c2435c052dc2953d7bf09ebb19ae4878a5404
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-16 11:14:11 +0300

    03

--- ExamPrepLab/DataCenter/Rack.cs
+++ ExamPrepLab/DataCenter/Rack.cs
@@ -1,9 +1,56 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Text;
 
 namespace DataCenter
 {
     public class Rack
     {
-       
+        public int Slots { get; set; }
+        public List<Server> Servers { get; set; }
+        public int GetCount => Servers.Count;
+
+        public Rack(int slots)
+        {
+            this.Slots = slots;
+            Servers = new List<Server>();
+        }
+
+        public void AddServer(Server server)
+        {
+            if(GetCount < Slots && !Servers.Any(s => s.SerialNumber == server.SerialNumber))
+            {
+                Servers.Add(server);    
+            }
+        }
+
+        public bool RemoveServer(string serialNumber)
+        {
+            Server server = Servers.FirstOrDefault(s => s.SerialNumber == serialNumber);
+            return Servers.Remove(server);
+        }
+
+        public string GetHighestPowerUsage()
+        {
+            Server server = Servers.MaxBy(s => s.PowerUsage);
+            return server.ToString();
+        }
+
+        public int GetTotalCapacity()
+        {
+            return Servers.Sum(s => s.Capacity);
+        }
+
+        public string DeviceManager()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{GetCount} servers operating:");
+            foreach (Server server in Servers)
+            {
+                sb.AppendLine(server.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
--- ExamPrepLab/DataCenter/Server.cs
+++ ExamPrepLab/DataCenter/Server.cs
@@ -2,6 +2,22 @@
 {
     public class Server
     {
-       
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public int PowerUsage { get; set; }
+
+        public Server(string serialNumber, string model, int capacity, int powerUsage)
+        {
+            SerialNumber = serialNumber;
+            Model = model;
+            Capacity = capacity;
+            PowerUsage = powerUsage;
+        }
+
+        public override string ToString()
+        {
+            return $"Server {SerialNumber}: {Model}, {Capacity}TB, {PowerUsage}W";
+        }
     }
 }
