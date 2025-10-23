c3493e42d6a90f124d634d6275febd2110b427cc
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-23 12:06:20 +0300

    04

---
+++ OOPCS/InheritanceLab/CustomRandomLis/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList()
+            {
+                "House", "Wow", "Meow"
+            };
+
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.Count);
+        }
+    }
+}
---
+++ OOPCS/InheritanceLab/CustomRandomLis/RandomList.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int randomIndex = random.Next(0,this.Count);
+            string value = this[randomIndex];
+
+            this.RemoveAt(randomIndex);
+            return value;
+        }
+    }
+}
