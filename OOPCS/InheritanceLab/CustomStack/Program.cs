3b41dbd0e61de70deff8a1ce261b611c3a6a403c
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-23 12:17:33 +0300

    05

---
+++ OOPCS/InheritanceLab/CustomStack/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings strings = new StackOfStrings();
+
+            Console.WriteLine(strings.IsEmpty());
+
+            Stack<string> values = new Stack<string>();
+            values.Push("hello");
+            values.Push("there");
+
+            strings.AddRange(values);
+        }
+    }
+}
---
+++ OOPCS/InheritanceLab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(Stack<string> strings)
+        {
+            foreach (var value in strings)
+            {
+                this.Push(value);
+            }
+        }
+    }
+}
