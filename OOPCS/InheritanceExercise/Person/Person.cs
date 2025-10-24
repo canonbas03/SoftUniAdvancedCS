6bdefe818c295c14810009e036d86aa5d7068efd
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-24 17:51:05 +0300

    01

---
+++ OOPCS/InheritanceExercise/Person/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name { get; set; }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public virtual int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if(value > 0)
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} -> Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
