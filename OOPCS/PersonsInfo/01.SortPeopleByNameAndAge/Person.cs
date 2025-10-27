b7a77160e1ac9e0926e1b4e0fa9813cbe0eecea8
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-10-27 17:11:14 +0300

    03

--- OOPCS/PersonsInfo/01.SortPeopleByNameAndAge/Person.cs
+++ OOPCS/PersonsInfo/01.SortPeopleByNameAndAge/Person.cs
@@ -8,6 +8,10 @@ namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
         public Person(string firstName, string lastName, int age, decimal salary)
         {
             this.FirstName = firstName;
@@ -15,15 +19,55 @@ namespace PersonsInfo
             this.Age = age;
             Salary = salary;
         }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
+        public string FirstName
+        {
+            get => this.firstName;
 
-        public decimal Salary { get; private set; }
+            private set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => this.lastName;
+
+            private set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                this.age = value;
+            }
+
+        }
+
+        public decimal Salary
+        {
+            get => this.salary; 
+            private set
+            {
+                if(value < 650)
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                this.salary = value;
+            }
+        }
 
         public void IncreaseSalary(decimal percentage)
         {
-            if(this.Age < 30)
+            if (this.Age < 30)
             {
                 percentage /= 2;
             }
