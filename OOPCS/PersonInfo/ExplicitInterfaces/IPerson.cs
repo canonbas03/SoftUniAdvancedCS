776e68b729854d2c13d00339269d224e676c9028
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-06 09:27:18 +0200

    08

---
+++ OOPCS/PersonInfo/ExplicitInterfaces/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaces
+{
+    public interface IPerson
+    {
+        string Name { get; }
+        int Age { get; }
+        string Country { get; }
+
+        string GetName();
+    }
+}
---
+++ OOPCS/PersonInfo/ExplicitInterfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExplicitInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                IPerson person = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+                Console.WriteLine( person.GetName());
+
+                IResident resident = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
