d222557437193f6fd650bf61d9ef219416da57a9
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-09 09:50:02 +0200

    01

---
+++ OOPCS/ExceptionsAndErrorHandling/SquareRoot/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                if(number < 0)
+                {
+                    throw new Exception("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
