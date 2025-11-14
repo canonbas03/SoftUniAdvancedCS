9d17f95795bec17daeb6b872fffeb8154a747c55
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-14 08:48:21 +0200

    01

---
+++ OOPCS/SOLIDLab/P01.Stream_Progress/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {   
+            var stream = new StreamProgressInfo(new Music("","",100,5));
+            Console.WriteLine(stream.CalculateCurrentPercent());
+        }
+    }
+}
