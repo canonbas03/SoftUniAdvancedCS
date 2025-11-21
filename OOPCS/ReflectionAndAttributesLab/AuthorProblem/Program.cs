edd04fc4299fb8884ea76670f237de8bbe9881f1
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-21 20:20:22 +0200

    05

---
+++ OOPCS/ReflectionAndAttributesLab/AuthorProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthorProblem
+{
+    internal class Program
+    {
+        [Author("Dzhan")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
