7920bd78dbe2aad076ac9a3452eacffb9f50386f
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-22 13:35:59 +0200

    02

---
+++ OOPCS/ReflectionAndAttributesExercise/ValidationAttributes/Utils/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes.Utils
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type type = obj.GetType();
+
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                object value = property.GetValue(obj);
+
+                var customValidationAttributes = property.GetCustomAttributes<MyValidationAttribute>();
+                foreach (var attribute in customValidationAttributes)
+                {
+                    if(!attribute.IsValid(value))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
